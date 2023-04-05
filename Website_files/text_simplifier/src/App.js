a01b7265fd9b1c4c16c9adacaab53d18b7ec1e4b
Naman Kaushik <kaushiknaman.dk@gmail.com>
2023-04-05 23:37:00 +0530

    reconfig

---
+++ Website_files/text_simplifier/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import HomePage from './HomePage';
+function App() {
+  return (
+    <HomePage />);
+}
+
+export default App;
