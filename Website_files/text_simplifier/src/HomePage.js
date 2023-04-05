bc97e48e8d9e0ab10e848b285c019bfa966b749b
sejalkardam <sejal20467@iiitd.ac.in>
2023-04-06 00:42:10 +0530

    navbar

--- Website_files/text_simplifier/src/HomePage.js
+++ Website_files/text_simplifier/src/HomePage.js
@@ -41,7 +41,16 @@ function HomePage() {
     };
     return (
         <div>
-            <h1 class="text-4xl my-5 font-bold text-center text-slate-600">Text Simplifier and Detoxifier</h1>
+            <h1 class="text-4xl my-5 font-bold text-center text-slate-600">Lexical Text Simplification and Detoxification</h1>
+            <div className="navbar">
+            <div className="menu" class="bg-sky-300 overflow-hidden w-128 top-0">
+                <div className="menu-items" class="text-2xl text-white" >
+                    <a href="#" class="float-left text-center px-3 py-1 block hover:bg-gray-300 hover:text-black">Home</a>
+                    <a href="#" class="float-left text-center px-3 py-1 block hover:bg-gray-300 hover:text-black">About Project</a>
+                    <a href="#" class="float-left text-center px-3 py-1 block hover:bg-gray-300 hover:text-black">Team Members</a>
+                </div>
+            </div>    
+            </div>
             <label class="relative block">
                 <input class="mx-[10%] my-[10%] placeholder:italic placeholder:text-slate-400 block bg-white w-[70%] h-[40%] border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Enter the text to be simplified..." type="text" name="search" onChange={changeText} />
             </label>
