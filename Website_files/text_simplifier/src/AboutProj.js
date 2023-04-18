fa7e97bac72780f8f7c2d94f0753ab2c3e7dfef8
Naman Kaushik <kaushiknaman.dk@gmail.com>
2023-04-18 22:16:15 +0530

    about screen

--- Website_files/text_simplifier/src/AboutProj.js
+++ Website_files/text_simplifier/src/AboutProj.js
@@ -3,8 +3,15 @@ import Navbar from './Navbar'
 
 export default function AboutProj() {
   return (
-      <div>
-          <Navbar/>
-          AboutProj</div>
+    <div>
+      <Navbar />
+
+      <div class="mx-auto my-8 max-w-2xl text-justify font-serif">
+        <p class="text-lg leading-relaxed">
+          The goal of this project is to make text more accessible to a wider audience, including those with cognitive or linguistic disabilities, as well as to promote more inclusive and respectful communication online.
+
+        </p>
+      </div>
+ि</div>
   )
 }
